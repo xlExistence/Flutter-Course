6b73ecae19653d7db3fb7c2af4dd7c3b94c05828
Josué Polina <josue.polina19122002@gmail.com>
2025-10-28 18:41:59 -0600

    sect: widget inside widget
    here we learned that the way we display things on the screen is, by order, using widgets objects that works with parent-children objects.
    we can add as much as we want an it should keep the order, also, we can set style to them

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -104,7 +104,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text(
+              'Haz presionado el botón:',
+              style: TextStyle(color: Colors.blue),
+              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
