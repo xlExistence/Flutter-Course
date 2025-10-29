9c8cff816b0bb16eb34822f9bd5edb4d0e343f37
Josué Polina <josue.polina19122002@gmail.com>
2025-10-28 18:56:41 -0600

    sect: widget vs argument
    Widget is a capital letter (Center, Text, Colors...)
    Argument is a lowercase letter (child, style, color...)
    
    the structure goes widget => argument => widget => argument... and so on

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -86,37 +86,9 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Haz presionado el botón:',
-              style: TextStyle(color: Colors.blue),
-              ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Text(
+          "Hello",
+          style: TextStyle(color: Colors.amber),
         ),
       ),
       floatingActionButton: FloatingActionButton(
